6296de03270a535f8bec2b787292ab3add61311d
khadija <khadijamahanga@y7mail.com>
2019-04-07 19:52:49 +0300

    add plot function and sigmoid function

--- week3/machine-learning-ex2/ex2/plotData.m
+++ week3/machine-learning-ex2/ex2/plotData.m
@@ -12,10 +12,10 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 % Find Indices of Positive and Negative Examples
-positive_points = find(y==1); negative_points = find(y == 0);
+positive_points = find(y==1);
+negative_points = find(y == 0);
 
-
-%
+%plox positives then plot negative points
 plot(X(positive_points, 1), X(positive_points, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
 plot(X(negative_points, 1), X(negative_points, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
