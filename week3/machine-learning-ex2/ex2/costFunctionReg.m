03b7fba934b348efb672ff45818d839dbed22bc6
khadija <khadijamahanga@y7mail.com>
2019-04-08 23:32:57 +0300

    logistic regression with regularization

--- week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h_theta = sigmoid(X*theta);
 
+
+shift_theta = theta(2:size(theta));
+theta_jey = [0;shift_theta];
+
+J = (1/m)* (-y'*log(h_theta) - (1 - y)'*log(1 - h_theta)) +(lambda/(2*m))*theta_jey'*theta_jey;
+
+grad = (1/m)*(X'*(h_theta-y)+lambda*theta_jey);
 
 
 
