443f893a921fac8b4ff13034cc3aa88dc3869eb7
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-04-17 15:06:20 +0300

    add prediction using the model + submit

--- week4/machine-learning-ex3/ex3/predict.m
+++ week4/machine-learning-ex3/ex3/predict.m
@@ -23,6 +23,34 @@ p = zeros(size(X, 1), 1);
 
 
 
+% Input Layer
+a1 = [ones(m, 1), X]; % results in [5000, 401]
+
+z2 = Theta1 * a1'; % results in [25, 5000]
+
+% Hidden Layer
+a2 = sigmoid(z2);  % results in [25, 5000]
+
+a2 = [ones(1, size(a2, 2)); a2]; % results in [26, 5000]
+
+% Output layer
+z3 = Theta2 * a2; % results in [10, 5000]
+
+a3 = sigmoid(z3); % results in [10, 5000]
+
+% calculating max on the transpose of a3 so the index 
+% result, p, has the expected dimensions [5000, 1]
+[val, p] = max(a3', [], 2);
+
+
+% or
+
+X = [ones(m, 1) X];
+a2 = sigmoid(X * Theta1');
+a2 = [ones(m, 1) a2];
+htheta = sigmoid(a2 * Theta2');
+
+[temp, p] = max(htheta, [], 2);
 
 
 
